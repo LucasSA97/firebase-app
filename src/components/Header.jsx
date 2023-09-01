78a524c82683e6d13952fdcdbd44085b74026793
LucasSA97 <lucassarachu7@gmail.com>
2023-09-01 09:23:37 +0200

    Github Action actualizada para aceptar warnings

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,6 @@ import { AppContext } from "../App";
 import { getAuth, signOut } from "firebase/auth";
 import { toast } from "react-hot-toast";
 
-
-
 const Header = () => {
   const auth = getAuth();
   const { setRoute, user, setUser } = useContext(AppContext);
@@ -14,14 +12,14 @@ const Header = () => {
       .then(() => {
         // Sign-out successful.
         setRoute("login");
-        setUser(null)
-        toast(`Hasta la proxima!`)
+        setUser(null);
+        toast(`Hasta la proxima!`);
       })
       .catch((error) => {
         // An error happened.
-        console.error(error)
+        console.error(error);
       });
-  }
+  };
   return (
     <header className="h-20 w-full bg-gray-100 shadow-lg flex items-center justify-between px-8 fixed top-0">
       <div
@@ -29,7 +27,9 @@ const Header = () => {
         onClick={() => setRoute("home")}
       >
         <SiFirebase className="text-2xl text-pink-600" />
-        <span className="text-xl font-semibold text-pink-600">Fire-Shop V2</span>
+        <span className="text-xl font-semibold text-pink-600">
+          Fire-Shop V2
+        </span>
       </div>
       <div className="flex gap-2">
         {user ? (
